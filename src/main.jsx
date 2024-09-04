212f4534c0a9c835b12685cdee58ba47ce12e530
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-04 23:51:41 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import { createElement } from 'react';
+import { createElement } from "react"; //React
+import { createRoot } from "react-dom/client"; //ReactDOM
 // import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 
 const element = createElement(
   "a",
   { href: "https://zrozumiecreact.pl" },
-  "Test"
+  "test"
 );
 
-createRoot(document.getElementById('root')).render(element);
+const elementInJSX = <a href="https://zrozumiecreact.pl">test 3</a>;
+console.log(<h1>test 2</h1>); // -> createElement('h1', {}, 'test 2')
+console.log(element);
+console.log(elementInJSX);
+
+createRoot(document.getElementById("root")).render(element);
