555b4e96a09f04cc927fc62bcca635550561d2df
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-04 23:38:28 +0100

    Update main.jsx to render a link element

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createElement } from 'react';
+// import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const element = createElement(
+  "a",
+  { href: "https://zrozumiecreact.pl" },
+  "Test"
+);
+
+createRoot(document.getElementById('root')).render(element);
