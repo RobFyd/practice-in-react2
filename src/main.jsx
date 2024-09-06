01b41239166603e05c01b7ce465fd8db59047411
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-06 23:10:05 +0100

    components exercises

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,24 @@
 // import { createElement } from "react"; //React.
 import { createRoot } from "react-dom/client"; //ReactDOM.
 
-const element = <div></div>;
+const element = (
+  <>
+    <h1>Our website</h1>
+    <main>
+      <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+        Course page
+      </a>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quasi
+        amet earum ullam, ipsa qui aut aliquam adipisci dolorum veniam
+        asperiores, placeat eveniet corporis eaque exercitationem, ab voluptatum
+        nisi necessitatibus?
+      </p>
+      <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+        Course page
+      </a>
+    </main>
+  </>
+);
 
 createRoot(document.getElementById("root")).render(element);
