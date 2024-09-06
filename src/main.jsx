ced0aeba5844a8b1231a06392279ea1bbc2b2d85
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-06 23:15:44 +0100

    linkElement Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,24 @@
 // import { createElement } from "react"; //React.
 import { createRoot } from "react-dom/client"; //ReactDOM.
 
+const linkElement = (
+  <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+    Awesome course page
+  </a>
+);
+
 const element = (
   <>
     <h1>Our website</h1>
     <main>
-      <a href="https://google.com" target="_blank" rel="noopener noreferrer">
-        Course page
-      </a>
+      {linkElement}
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quasi
         amet earum ullam, ipsa qui aut aliquam adipisci dolorum veniam
         asperiores, placeat eveniet corporis eaque exercitationem, ab voluptatum
         nisi necessitatibus?
       </p>
-      <a href="https://google.com" target="_blank" rel="noopener noreferrer">
-        Course page
-      </a>
+      {linkElement}
     </main>
   </>
 );
