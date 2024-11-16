8f6e626a6281d6983ff6510f1945d21969a4926c
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-11-16 00:01:53 +0000

    Add imgWidth prop to Animal component for flexible image sizing

--- exercises/Animal.jsx
+++ exercises/Animal.jsx
@@ -1,8 +1,8 @@
-export function Animal({ name, imgSrc }) {
+export function Animal({ name, imgSrc, imgWidth = 200 }) {
   return (
     <section>
       <h2>{name}</h2>
-      <img src={imgSrc} alt={name} width={200} />
+      <img src={imgSrc} alt={name} width={imgWidth} />
     </section>
   );
 }
