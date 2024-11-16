4f9f4fa13cc4800e6e52f66a33f97e64932a23bb
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-11-16 00:02:00 +0000

    Add imgWidth prop to Animal instances for consistent image sizing

--- exercises/main.jsx
+++ exercises/main.jsx
@@ -1,12 +1,21 @@
 import ReactDOM from "react-dom/client";
-import { Animal } from "/Animal.jsx";
+import { Animal } from "./Animal";
 
 const element = (
   <>
     <h1>Galeria zwierząt</h1>
-    <Animal name="Pies" imgSrc="https://zrozumiecreact.pl/dog.jpg" />
-    <Animal name="Kot" imgSrc="https://zrozumiecreact.pl/cat.jpg" />
+    <Animal
+      name="Pies"
+      imgSrc="https://zrozumiecreact.pl/dog.jpg"
+      imgWidth={300}
+    />
+    <Animal
+      name="Kot"
+      imgSrc="https://zrozumiecreact.pl/cat.jpg"
+      imgWidth={250}
+    />
     <Animal name="Kaczka" imgSrc="https://zrozumiecreact.pl/duck.jpg" />
   </>
 );
+
 ReactDOM.createRoot(document.getElementById("root")).render(element);
